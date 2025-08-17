8a4275a76ad9bde6007bddc333719e34fc15e318
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-08-17 15:08:44 -0300

    feat: adicionar swagger na biblioteca e rota para ver na web

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaClient } from '@prisma/client';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3333);
 
+  setupSwagger(app);
   await connectToDatabase();
+
+  await app.listen(process.env.PORT ?? 3333);
 }
 
 async function connectToDatabase() {
@@ -16,6 +19,27 @@ async function connectToDatabase() {
     console.log('Connected to the database');
   } catch (error) {
     console.error('Error connecting to the database:', error);
+  }
+}
+
+function setupSwagger(app: any) {
+  try {
+    const config = new DocumentBuilder()
+      .setTitle('Escola API')
+      .setDescription('API para gerenciamento de uma escola')
+      .setVersion('1.0')
+      .addTag('escola')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+    if (!process.env.PORT) {
+      process.env.PORT = '3333';
+    }
+    console.log(
+      `Swagger is set up at http://localhost:${process.env.PORT}/docs`,
+    );
+  } catch (error) {
+    console.error('Error setting up Swagger:', error);
   }
 }
 
